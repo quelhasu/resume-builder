674da86c5030b7ebab43bd61e679c77978e7af2a
quelhasu <uquelhas@gmail.com>
2020-04-29 19:21:44 +0200

    feat: structure

---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext } from 'react';
+
+const Preview = () => {
+  return (
+      <div className="h-screen col-span-3 relative overflow-hidden flex justify-center items-center">
+        <p>Hello</p>
+      </div>
+  )
+}
+
+export default Preview
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext } from 'react';
+
+const Sidebar = () => {
+  return (
+    <div id="sidebar" className="h-screen col-span-2 py-8 shadow-xl">
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default Sidebar;
